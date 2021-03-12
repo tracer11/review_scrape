0404c9059a10c161f34cce705347c5d20c3d211f
Karol Gurba <karolgurba22@gmail.com>
2021-03-12 12:02:43 -0600

    made changes to review model

--- scraper/models.py
+++ scraper/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.validators import MaxValueValidator
 
 
 #fields = {title of the review: Char, the content of review: TextField, author: Char, star rating : float, date of review : date , closed with lender : bool }
@@ -8,9 +7,8 @@ class Review(models.Model):
   title = models.CharField(max_length=250)
   content = models.TextField(max_length=1000)
   author = models.CharField(max_length=250)
-  star_rating = models.FloatField(validators=[MaxValueValidator])
+  star_rating = models.CharField(max_length=250)
   date_of_review = models.CharField(max_length=250)
-  closed_with_lender = models.BooleanField()
 
 
     
